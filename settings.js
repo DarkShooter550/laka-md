fe26556eede86a5cdf3abc0a277180015419b7c1
DarkShooter550 <132283700+DarkShooter550@users.noreply.github.com>
2025-02-06 22:20:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-// LAKA-MD=tl5DhZyQ#Hy9UracRtM-W_3vMpILxOIzoWQo_RGALIDLrHqQBZr0
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=tl5DhZyQ#Hy9UracRtM-W_3vMpILxOIzoWQo_RGALIDLrHqQBZr0",
-// mongodb+srv://darkshooter550:kQIoUmay53jKPWYv@cluster0.m2hzt.mongodb.net/
+// LAKA-MD=phYwmYKJ#URvdrLLOOdRw8rIRmq9dYDU3JwPyEUfPld_wAdgkbAw
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=phYwmYKJ#URvdrLLOOdRw8rIRmq9dYDU3JwPyEUfPld_wAdgkbAw",
+// mongodb+srv://darkshooter550:kQIoUmay53jKPWYvmongodb.net/
 MONGODB: process.env.MONGODB || "mongodb+srv://darkshooter550:kQIoUmay53jKPWYv@cluster0.m2hzt.mongodb.net/",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
